a5f4ec4cb3cf17c87f90b2253e6caf6468e7a854
joseph <josephmaary@outlook.com>
2021-02-11 22:26:25 +0200

    chnage the router.delete() function to router.get()

--- router.js
+++ router.js
@@ -7,13 +7,12 @@ router.get('/', (req, res, next) => { res.render('homepage')})
 router.get('/filter-certificates', (req, res, next) => { res.render('filter')})
 router.get('/add-certificates', (req, res, next) => { res.render('add')})
 router.get('/certificate-details/:id', certificateController.showCertificateDetails)
+router.get('/delete/:id', certificateController.deleteCertificate)
+
 
 // post routes
 router.post('/filter', certificateController.showCertificates)
 router.post('/add', certificateController.addCertificate)
 
-
-//delete routes
-router.delete('/delete/:id', certificateController.deleteCertificate)
 
 module.exports = router;
